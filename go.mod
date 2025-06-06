d89e86722ecaaa11444b20ebf41d5428bd0bb5d8
Roger Westerbø <roger.westerbo@gmail.com>
2025-06-06 08:33:48 +0200

    Upgrading golang dependencies Adding machine

--- go.mod
+++ go.mod
@@ -8,21 +8,24 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fxamacker/cbor/v2 v2.8.0 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
-	golang.org/x/net v0.40.0 // indirect
-	golang.org/x/text v0.25.0 // indirect
+	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
+	k8s.io/api v0.33.1 // indirect
 	k8s.io/klog/v2 v2.130.1 // indirect
-	k8s.io/utils v0.0.0-20250502105355-0f33e8f1c979 // indirect
+	k8s.io/utils v0.0.0-20250604170112-4c0f3b243397 // indirect
 	sigs.k8s.io/json v0.0.0-20241014173422-cfa47c3a1cc8 // indirect
 	sigs.k8s.io/randfill v1.0.0 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.7.0 // indirect
---
+++ pkg/v1alpha1/machine.go
@@ -0,0 +1,93 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type Machine struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MachineSpec   `json:"spec,omitempty"`
+	Status MachineStatus `json:"status,omitempty"`
+}
+
+type MachineSpec struct {
+	// The name of the machine
+	Name string `json:"name"`
+	// The type of the machine
+	Type string `json:"type"`
+}
+
+type MachineStatus struct {
+	// The unique identifier for the machine
+	MachineID string `json:"machineID"`
+
+	// systemd identifier for the machine
+	SystemdID string `json:"systemdID"`
+
+	// The current state of the machine
+	State string `json:"state"`
+
+	// The current status of the machine
+	Status string `json:"status"`
+
+	// The last time the machine status was updated
+	LastUpdated metav1.Time `json:"lastUpdated"`
+
+	// The provider of the machine
+	Provider string `json:"provider"`
+
+	// The region where the machine is located
+	Region string `json:"region"`
+
+	// The zone where the machine is located
+	Zone string `json:"zone"`
+
+	// The IP address of the machine
+	IPAddress []string `json:"ipAddress"`
+
+	Ipv6Address []string `json:"ipv6Address"`
+
+	// Thme machine's cpu architecture
+	Architecture string `json:"architecture"`
+
+	// The machine's operating system
+	OperatingSystem string `json:"operatingSystem"`
+
+	// The machine's operating system version
+	OperatingSystemVersion string `json:"operatingSystemVersion"`
+
+	// The machine's kernel version
+	KernelVersion string `json:"kernelVersion"`
+
+	// The machine's hostname
+	Hostname string `json:"hostname"`
+
+	// the machine's cpus
+	CPUs int `json:"cpus"`
+
+	// The machine's memory in bytes
+	Memory int64 `json:"memory"`
+
+	// The machine's disk size in bytes
+	Disks []MachineDisk `json:"disks"`
+}
+
+type MachineDisk struct {
+	// The disk's name
+	Name string `json:"name"`
+	// The disk's size in bytes
+	Size int64 `json:"size"`
+	// The disk's type (e.g., SSD, HDD)
+	Type string `json:"type"`
+	// The disk's mount point
+	MountPoint string `json:"mountPoint"`
+	// The disk's filesystem type (e.g., ext4, xfs)
+	FilesystemType string `json:"filesystemType"`
+	// The disk's UUID
+	UUID string `json:"uuid"`
+	// The disk's label
+	Label string `json:"label"`
+	// The disk's serial number
+}
